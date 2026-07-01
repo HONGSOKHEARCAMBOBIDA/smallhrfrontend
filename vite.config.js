f28eed15934c05d5aebf493d7c5ada4c2c01ec40
HONGSOKHEARCAMBODIA <emmma7096@gmail.com>
2026-07-01 21:56:28 +0700

    update

--- vite.config.js
+++ vite.config.js
@@ -7,8 +7,8 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-      //  target: 'https://api-hr.familytech.dev',
-        target: 'http://localhost:8080',
+      target: 'https://api-hr.familytech.dev',
+        //  target: 'http://192.241.132.206:8080',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '')
       }
