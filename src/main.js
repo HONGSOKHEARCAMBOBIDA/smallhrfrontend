6e3529cdbf6eb85c58cd4f1297a4a42ec2f5995f
HONGSOKHEARCAMBODIA <emmma7096@gmail.com>
2026-07-05 16:39:55 +0700

    update

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import 'element-plus/dist/index.css'
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 import App from './App.vue'
 import router from './router'
+import { globalCookiesConfig } from "vue3-cookies";
 globalCookiesConfig({
   expireTimes: "30d",
   path: "/",
