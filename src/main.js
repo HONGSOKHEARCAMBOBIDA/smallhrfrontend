3a89937c29f20e470dfeef9a653ab51c7fde2aac
HONGSOKHEARCAMBODIA <emmma7096@gmail.com>
2026-07-05 16:37:59 +0700

    update

--- src/main.js
+++ src/main.js
@@ -5,7 +5,13 @@ import 'element-plus/dist/index.css'
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 import App from './App.vue'
 import router from './router'
-
+globalCookiesConfig({
+  expireTimes: "30d",
+  path: "/",
+  domain: "https://api-hr.familytech.dev",
+  secure: true,              
+  sameSite: "None"          
+});
 const app = createApp(App)
 
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
